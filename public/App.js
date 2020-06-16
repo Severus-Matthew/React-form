1f9567fe150b5038f463f467bd52c6e8103498c5
Severus-Matthew <55323775+Severus-Matthew@users.noreply.github.com>
2020-06-16 18:42:02 +0530

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './profile.css';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className =  "company">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
